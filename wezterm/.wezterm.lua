f0f2545a048b02793d8d3ab8c9eb345f4976e9b3
toilavinh <hoangdvinh68@gmail.com>
2025-01-16 00:48:46 +0700

    Update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,17 +1,37 @@
+-- __      __      _
+-- \ \    / /__ __| |_ ___ _ _ _ __
+--  \ \/\/ / -_)_ /  _/ -_) '_| '  \
+--   \_/\_/\___/__|\__\___|_| |_|_|_|
+
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = wezterm.config_builder()
 
+-- Core
 config.automatically_reload_config = true
 config.check_for_updates = false
-
 config.default_prog = { "powershell.exe", "-NoLogo" }
 config.default_cursor_style = 'SteadyUnderline'
 
+-- Performances
 config.max_fps = 120
 config.prefer_egl = true
 config.front_end = "OpenGL"
 config.animation_fps = 1
 
+-- Colors
+config.color_scheme = 'Catppuccin Mocha'
+
+-- Fonts
+-- config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Bold' })
+config.font = wezterm.font_with_fallback({
+    { family = "JetBrainsMono Nerd Font", scale = 1, weight = "Bold", },
+})
+config.font_size = 9
+config.line_height = 1
+config.adjust_window_size_when_changing_font_size = false
+
+-- Windows
 config.window_decorations = "RESIZE"
 config.window_padding = {
     left = 0,
@@ -20,15 +40,58 @@ config.window_padding = {
     bottom = 0,
 }
 config.window_background_opacity = 0.9
-config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.scrollback_lines = 3000
+config.initial_cols = 150
+config.initial_rows = 40
+
+-- Tabbars
+config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
-config.initial_cols = 150
+config.inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.8,
+}
+config.status_update_interval = 1000
+wezterm.on("update-status", function(window, pane)
+    -- Normalize path
+    local basename = function(s)
+      -- Nothing a little regex can't fix
+      return string.gsub(s, "(.*[/\\])(.*)", "%2")
+    end
 
-config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Medium' })
-config.font_size = 9
-config.line_height = 1
-config.adjust_window_size_when_changing_font_size = false
-config.color_scheme = 'Catppuccin Mocha'
+    -- Print working directory
+    local pwd = pane:get_current_working_dir()
+    if pwd then
+      if type(pwd) == "userdata" then
+        pwd = basename(pwd.file_path)
+      else
+        pwd = basename(pwd)
+      end
+    else
+        pwd = ""
+    end
+
+    -- Current shell
+    local cmd = pane:get_foreground_process_name()
+    cmd = cmd and basename(cmd) or ""
+
+    -- Current time
+    local time = wezterm.strftime("%H:%M")
+  
+    -- Right status
+    window:set_right_status(wezterm.format({
+      { Text = wezterm.nerdfonts.md_folder .. "  " .. pwd },
+      { Text = " | " },
+      { Foreground = { Color = "#e0af68" } },
+      { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+      "ResetAttributes",
+      { Text = " | " },
+      { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+      { Text = "  " },
+    }))
+  end
+)
 
 return config
